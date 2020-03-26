d307216c752aeff89780e3bb0bc26e87947c5872
Ben Little <ben.little6@gmail.com>
2020-03-26 15:51:54 -0600

    Start playing with layouts

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,51 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
+    <TheNavigationBar/>
     <v-content>
-      <HelloWorld/>
+      <TitleCard
+        :avatar="avatar"
+        :name="name"
+        :title="title"
+        :tagline="tagline"
+      />
+      <Highlights
+        showRecentActivity=showRecentActivity
+      />
+      <Projects
+        projects=projects
+      />
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import TheNavigationBar from './components/TheNavigationBar';
+import TitleCard from './components/TitleCard';
+import Highlights from './components/Highlights';
+import Projects from './components/Projects';
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    TheNavigationBar,
+    TitleCard,
+    Highlights,
+    Projects,
   },
 
   data: () => ({
-    //
+    avatar: "assets/avatar.png",
+    name: "Ben Little",
+    title: "Software Engineer",
+    tagline: "Mindful, Mathematical, Modular",
+    showRecentActivity: true,
+    projects: [
+      {
+        title: "Raku in the Browser",
+        link: "raku-in-the-browser"
+      }
+    ],
   }),
 };
 </script>
---
+++ src/components/TitleCard.vue
@@ -0,0 +1,48 @@
+
+<template>
+  <v-container
+    fluid
+    class="blue d-flex"
+  >
+    <v-col
+      class="title"
+    >
+      <v-row class="justify-end">
+        <v-title
+          class="title justify-around"
+        >
+          {{ title }}
+        </v-title>
+        <v-subtitle
+          class="subtitle justify-around"
+        >
+          {{ tagline }}
+        </v-subtitle>
+      </v-row>
+    </v-col>
+    <v-col
+      class="title"
+    >
+      <v-avatar>
+        <img :src="avatar">
+      </v-avatar>      
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'TitleCard',
+  props: [
+    'avatar',
+    'name',
+    'title',
+    'tagline',
+  ],
+  data: () => {
+
+  }
+}
+
+</script>
+
