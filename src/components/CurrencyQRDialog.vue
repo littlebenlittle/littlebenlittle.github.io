7991b894c4f4a9259d5c07a9835f2afe212d1ea3
Ben Little <ben.little6@gmail.com>
2020-03-31 14:09:29 -0600

    Add QR dialog component

---
+++ src/components/CurrencyQRDialog.vue
@@ -0,0 +1,40 @@
+
+<template>
+    <v-dialog
+      max-width="290"
+      v-model="show"
+    >
+
+      <v-card>
+        <v-card-title class="headline">
+          {{ name }}
+        </v-card-title>
+        <v-img
+         :src="src"
+        >
+        </v-img>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+            @click="$emit('done')"
+          >
+              Done
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  data: () => { return {} },
+  props: {
+      name : String,
+      show : Boolean,
+      src  : String,
+  },
+}
+
+</script>
+
