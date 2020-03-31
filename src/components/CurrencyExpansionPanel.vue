f183feb3f9bed299058d5f430e1183b1f2df785c
Ben Little <ben.little6@gmail.com>
2020-03-31 14:08:09 -0600

    Add currency expansion panel component

---
+++ src/components/CurrencyExpansionPanel.vue
@@ -0,0 +1,63 @@
+<template>
+    <v-expansion-panel >
+
+      <v-expansion-panel-header>
+          <v-row
+            align="center"
+          >
+            <v-col
+              cols=2
+            >
+              <v-avatar
+                size="24"
+              >
+                <img
+                 :src="icon"
+                />
+              </v-avatar>
+            </v-col>
+            <v-col
+            >
+              <div
+                class="title"
+              >
+                {{ name }}
+              </div>
+            </v-col>
+          </v-row>
+      </v-expansion-panel-header>
+
+      <v-expansion-panel-content>
+        <v-container>
+          <v-row
+            align="center"
+          >
+            <v-col
+              cols=2
+            >
+              <v-btn
+                rounded
+                @click.stop="$emit('show-qr')"
+              >
+                Show QR
+              </v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-expansion-panel-content>
+
+    </v-expansion-panel>
+</template>
+
+<script>
+export default {
+  data: () => { return {
+  }},
+  props: {
+      name : String,
+      icon : String,
+  },
+}
+
+</script>
+
---
+++ src/components/CurrencyExpansionPanelGroup.vue
@@ -0,0 +1,70 @@
+
+<template>
+  <v-container>
+
+    <CurrencyQRDialog
+      v-for="cur in currencies"
+     :key="cur.name + '-dialog'" 
+     :name="cur.name"
+     :show="cur.qr.show"
+     :src="cur.qr.url"
+      v-on:done="cur.qr.show = false"
+    />
+
+    <v-expansion-panels>
+      <CurrencyExpansionPanel
+        v-for="cur in currencies"
+       :key="cur.name + 'expansion-panel'"
+       :name="cur.name"
+       :icon="cur.icon.url"
+        v-on:show-qr="cur.qr.show = true"
+      />
+    </v-expansion-panels>
+
+  </v-container>
+</template>
+
+<script>
+
+import btcqr   from "../assets/btc-qr.svg"
+import ethqr   from "../assets/eth-qr.svg"
+import btcicon from "../assets/btc-icon.svg"
+import ethicon from "../assets/eth-icon.svg"
+
+import CurrencyExpansionPanel from "./CurrencyExpansionPanel"
+import CurrencyQRDialog from "./CurrencyQRDialog"
+
+export default {
+  name: 'CurrencyExpansionPanels',
+  components: {
+    CurrencyExpansionPanel,
+    CurrencyQRDialog,
+  },
+  data: () => { return {
+    currencies: [
+      {
+        name: 'Bitcoin',
+        qr: {
+          show : false,
+          url  : btcqr,
+        },
+        icon: {
+          url  : btcicon,
+        },
+      },
+      {
+        name: 'Ethereum',
+        qr: {
+          show : false,
+          url  : ethqr,
+        },
+        icon: {
+          url  : ethicon,
+        },
+      },
+    ]
+  }},
+}
+
+</script>
+
